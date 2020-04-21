f1c2ad2581f2c3269861b06d2701c19771bf89b8
nmehanma <nmehanma@gmail.com>
2020-04-20 21:38:52 -0400

    updating our checklist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ToDoItem from './components/ToDoItem'
+import todosData from './components/todosData'
 import './components/style.css'
 
 function App() {
+
+  const todoItems= todosData.map(item => <ToDoItem key={item.id} item={item}/>)
+
   return (
     <div className="todo-list">
-      <ToDoItem />
-      <ToDoItem />
-      <ToDoItem />
-      <ToDoItem />
+      {todoItems}
     </div>
   )
 }
