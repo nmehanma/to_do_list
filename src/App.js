f0600fca044941700a6052c2263ce944202ea880
nmehanma <nmehanma@gmail.com>
2020-04-21 20:58:30 -0400

    modified handleChange set state confusion

--- src/App.js
+++ src/App.js
@@ -3,15 +3,43 @@ import ToDoItem from './components/ToDoItem'
 import todosData from './components/todosData'
 import './components/style.css'
 
-function App() {
-
-  const todoItems= todosData.map(item => <ToDoItem key={item.id} item={item}/>)
-
-  return (
-    <div className="todo-list">
-      {todoItems}
-    </div>
-  )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo
+            })
+            console.log(prevState.todos)
+            console.log(updatedTodos)
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+    
+    render() {
+        const todoItems = this.state.todos.map(item => <ToDoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )    
+    }
 }
 
 export default App
